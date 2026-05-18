884957a22d890809d235ab11ccdb52d771d644b9
wf5663893-gif <wf5663893@gmail.com>
2026-05-19 00:31:16 +0700

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA_rvuqRf30X62wy-vLXOgiAwhcvMNyvs0",
+  authDomain: "videostream-dabfe.firebaseapp.com",
+  projectId: "videostream-dabfe",
+  storageBucket: "videostream-dabfe.firebasestorage.app",
+  messagingSenderId: "26769321765",
+  appId: "1:26769321765:web:d24e42712e03069d7d34cc",
+  measurementId: "G-CYJGPR95J9"
+};
